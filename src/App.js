8a619510e76fb2b3b0f510a7095e5fe45f7af8eb
Kyle Roach <kroach.work@gmail.com>
2019-07-13 03:39:37 -0400

    feat: Setup rnw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { SocialIcon } from "react-native-elements";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style type="text/css">{`
+        @font-face {
+          font-family: 'MaterialIcons';
+          src: url(${require("react-native-vector-icons/Fonts/MaterialIcons.ttf")}) format('truetype');
+        }
+
+        @font-face {
+          font-family: 'FontAwesome';
+          src: url(${require("react-native-vector-icons/Fonts/FontAwesome.ttf")}) format('truetype');
+        }
+      `}</style>
+
+      <div className="App">
+        <header className="App-header">
+          <SocialIcon
+            title="Sign In With Facebook"
+            button={true}
+            type="facebook"
+            onPress={() => {}}
+          />
+        </header>
+      </div>
+    </>
   );
 }
 
